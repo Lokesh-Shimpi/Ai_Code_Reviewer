49ab2522bc8c3f324cd2ca715d09083d4aaafa64
Lokesh-Shimpi <167664213+Lokesh-Shimpi@users.noreply.github.com>
2025-06-21 10:48:24 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,4 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 4001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
